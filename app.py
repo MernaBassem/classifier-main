7308ca39e66f0d10c6b3160e4d54d3df624f5d60
MernaBassem <96470684+MernaBassem@users.noreply.github.com>
2023-04-15 06:23:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import cv2
 app = Flask(__name__)
 
 cancer_cells = ['malignant', 'benine']
-model = load_model('D:/intel-Classfier-main/MLmodellast.pbtxt')
+model = load_model('D:\intel-Classfier-main\MLmodellast.h5')
 
 
 @app.route('/')
