391e7aac24e25f82f0769e087bbf8d68b37e05d2
MernaBassem <96470684+MernaBassem@users.noreply.github.com>
2023-04-16 05:18:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ import cv2
 
 app = Flask(__name__)
 
-cancer_cells = ['malignant','benine']
-model=load_model('MLmodellast.h5')
+
+
+classes = ['malignant' ,'benine']
+model=load_model("MLmodellast.h5")
 
 @app.route('/')
 def index():
@@ -23,34 +25,20 @@ def api():
 
         if 'fileup' not in request.files:
             return "Please try again. The Image doesn't exist"
-        image = request.files.get('fileup')
-        # convert string data to numpy array
 
 
         img_data = request.files['fileup'].read()
-
-# Convert binary data to numpy array
         nparr = np.frombuffer(img_data, np.uint8)
-
-# Decode image from numpy array
-        img_np = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
-        img_np = cv2.cvtColor(img_np, cv2.COLOR_GRAY2RGB)
+        img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+        img_np = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
         img_np = cv2.resize(img_np, (70, 70))
         img_np = img_np / 255.0
         img_np = np.expand_dims(img_np, axis=0)
-# make prediction using the loaded model
-        pred_probs = model.predict(img_np)[0]
-        pred_class = np.argmax(pred_probs)
-        pred_label = cancer_cells[pred_class]
-
-
-
-
         print("Model predicting ...")
         result = model.predict(img_np)
         print("Model predicted")
         ind = np.argmax(result)
-        prediction = cancer_cells[ind]
+        prediction = classes[ind]
         print(result)
         print(prediction)
         return jsonify({'prediction': prediction})
@@ -62,35 +50,28 @@ def predict():
     print("run code")
     if request.method == 'POST':
         # Get the image from post request
-    
+        print("image loading....")
+        image = request.files['fileup']
         print("image loaded....")
-        img_data = request.files['fileup'].read()
-
-        # Convert binary data to numpy array
+        img_data = request.files.get('fileup')
         nparr = np.frombuffer(img_data, np.uint8)
-
         # Decode image from numpy array
         img_np = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
         img_np = cv2.cvtColor(img_np, cv2.COLOR_GRAY2RGB)
         img_np = cv2.resize(img_np, (70, 70))
         img_np = img_np / 255.0
         img_np = np.expand_dims(img_np, axis=0)
-        # make prediction using the loaded model
-        pred_probs = model.predict(img_np)[0]
-        pred_class = np.argmax(pred_probs)
-        pred_label = cancer_cells[pred_class]
-        # return predictions as JSON response
         print("predicting ...")
         result = model.predict(img_np)
         print("predicted ...")
         ind = np.argmax(result)
-        prediction = cancer_cells[ind]
+        prediction = classes[ind]
 
         print(prediction)
 
         return render_template('index.html', prediction=prediction, image='static/IMG/', appName="MLmodellast")
     else:
-        return render_template('index.html',appName="MLmodellast")
+        return render_template('index.html',appName="MLmodellast.")
 
 
 if __name__ == '__main__':
